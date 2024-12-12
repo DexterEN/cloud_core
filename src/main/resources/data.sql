3f7d3141a9978d3ad21ae8cc6165b0e62be51a4e
Dexter Nyika <129272732+DexterEN@users.noreply.github.com>
2024-12-12 12:51:44 +0100

    fixed app properties again

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO T_Location (city, address, postalCode)
+INSERT INTO t_location (city, address, postalCode)
 VALUES ('Stockholm City', 'Stockholm vägen 1', '116 60');
 
-INSERT INTO T_Organization (name, location_id)
+INSERT INTO t_organization (name, location_id)
 VALUES ('Stockholm AB', LAST_INSERT_ID());
 
 
-INSERT INTO T_Practitioner (name, dateOfBirth, role, email,organization_id)
+INSERT INTO t_practitioner (name, dateOfBirth, role, email,organization_id)
 VALUES ('test', '1980-01-01 00:00:00', 'DOCTOR', 'test@gmail.com',LAST_INSERT_ID());
 
-INSERT INTO T_Patient (gender, name, dateOfBirth, email)
+INSERT INTO t_patient (gender, name, dateOfBirth, email)
 VALUES ('MALE', 'patient', '2000-01-01 00:00:00', 'patient@gmail.com');
 

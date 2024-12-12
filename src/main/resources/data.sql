0b03645010beb2a26b98828f3a7bb0591f550ef1
Dexter Nyika <129272732+DexterEN@users.noreply.github.com>
2024-12-12 13:49:32 +0100

    fixed sql statements again!!!

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,15 @@
 -- Insert into t_location if it does not exist based on postal_code
-INSERT INTO t_location (city, address, postal_code)
-SELECT 'Stockholm City', 'Stockholm vägen 1', '116 60'
-WHERE NOT EXISTS (
-    SELECT 1 FROM t_location WHERE postal_code = '116 60'
-);
+INSERT IGNORE INTO t_location (city, address, postal_code)
+VALUES ('Stockholm City', 'Stockholm vägen 1', '116 60');
 
-INSERT INTO t_organization (name, location_id)
-SELECT 'Stockholm AB', LAST_INSERT_ID()
-WHERE NOT EXISTS (
-    SELECT 1 FROM t_organization WHERE name = 'Stockholm AB'
-);
+-- Insert into t_organization if it does not exist based on name
+INSERT IGNORE INTO t_organization (name, location_id)
+VALUES ('Stockholm AB', LAST_INSERT_ID());
 
-INSERT INTO t_practitioner (name, date_of_birth, role, email, organization_id)
-SELECT 'test', '1980-01-01 00:00:00', 'DOCTOR', 'test@gmail.com', LAST_INSERT_ID()
-WHERE NOT EXISTS (
-    SELECT 1 FROM t_practitioner WHERE email = 'test@gmail.com'
-);
+-- Insert into t_practitioner if it does not exist based on email
+INSERT IGNORE INTO t_practitioner (name, date_of_birth, role, email, organization_id)
+VALUES ('test', '1980-01-01 00:00:00', 'DOCTOR', 'test@gmail.com', LAST_INSERT_ID());
 
-INSERT INTO t_patient (gender, name, date_of_birth, email)
-SELECT 'MALE', 'patient', '2000-01-01 00:00:00', 'patient@gmail.com'
-WHERE NOT EXISTS (
-    SELECT 1 FROM t_patient WHERE email = 'patient@gmail.com'
-);
+-- Insert into t_patient if it does not exist based on email
+INSERT IGNORE INTO t_patient (gender, name, date_of_birth, email)
+VALUES ('MALE', 'patient', '2000-01-01 00:00:00', 'patient@gmail.com');
